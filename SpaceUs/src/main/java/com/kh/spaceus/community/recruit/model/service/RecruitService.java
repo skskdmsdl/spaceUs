aaafb0ec7cf7793872e1bb380767614ab57ae908
skskdmsdl <skskdmsdl@gmail.com>
2020-09-25 08:59:25 +0900

    구인구직 인서트 연결중

--- SpaceUs/src/main/java/com/kh/spaceus/community/recruit/model/service/RecruitService.java
+++ SpaceUs/src/main/java/com/kh/spaceus/community/recruit/model/service/RecruitService.java
@@ -3,4 +3,6 @@ package com.kh.spaceus.community.recruit.model.service;
 
 public interface RecruitService {
 
+	int insertRecruit(String header, String title, String ir1);
+
 }
--- SpaceUs/src/main/java/com/kh/spaceus/community/recruit/model/service/RecruitServiceImpl.java
+++ SpaceUs/src/main/java/com/kh/spaceus/community/recruit/model/service/RecruitServiceImpl.java
@@ -13,6 +13,11 @@ public class RecruitServiceImpl implements RecruitService{
 	@Autowired
 	private RecruitDAO recruitDAO;
 
+	@Override
+	public int insertRecruit(String header, String title, String ir1) {
+		return 0;
+	}
+
 	
 
 }
