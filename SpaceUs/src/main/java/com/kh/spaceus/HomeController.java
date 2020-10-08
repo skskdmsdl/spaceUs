56441817e7d2603290525681beaed5f1151d7d8d
UniceLee <april27thmon@gmail.com>
2020-10-08 14:18:55 +0900

    spacemapper, homecontroller

--- SpaceUs/src/main/java/com/kh/spaceus/HomeController.java
+++ SpaceUs/src/main/java/com/kh/spaceus/HomeController.java
@@ -28,7 +28,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-	
+		
 		
 		return "forward:/index.jsp";
 	}
